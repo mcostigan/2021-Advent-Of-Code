94375fd1b852f279f66048df3ae8a776414b2346
Matt Costigan <costigan@uchicago.edu>
2021-12-17 20:39:40 -0600

    adding comments and documentations

--- adventOfCode/day8/day8.py
+++ adventOfCode/day8/day8.py
@@ -16,6 +16,7 @@ def inputs(path):
 
 
 def part1():
+    # get count of unique readings in input
     results = 0
     for reading in readings:
         for result in readings[reading]:
@@ -25,49 +26,80 @@ def part1():
 
 
 def part2():
+    # create running sum
     answer = 0
+
+    # for each reading in input
     for reading in readings:
+        # map chars to correct chars {chars: correctchar}
         solution = {}
+
+        # get frequency analysis
         freq, uniq_freq = frequency_analysis(reading)
+
+        # loop through keys in freq
         for key in freq:
+            # if char is of unique frequency
             if len(frequency[freq[key]]) == 1:
+                # map solution
                 solution[key] = frequency[freq[key]][0]
             else:
+                # key is of a unique frequency among 1,4,7,8
                 if frequency[freq[key]] == ['g', 'd']:
+                    # if either g or d, d appears twice in 1,4,7,8
                     if uniq_freq[key] == 2:
                         solution[key] = 'd'
                     else:
                         solution[key] = 'g'
                 else:
+                    # and a appears twice in 1,4,7,8
                     if uniq_freq[key] == 2:
                         solution[key] = 'a'
                     else:
                         solution[key] = 'c'
+
+        # use solution to get resulting four digit display value
         result = 0
+        # for each digit
         for i in range(4):
             digit = readings[reading][i]
             s = ''
+            # for each letter in digit
             for char in digit:
+                # lookup in solution
                 s += solution[char]
+            # sort for consistency and lookup in mapping
             number = numbers[''.join(sorted(s))]
-            result += number * 10**(3-i)
-        answer += result
-    print(answer,'sum of all readings')
 
+            # add digit to result
+            result += number * 10 ** (3 - i)
+        # add result to final answer
+        answer += result
+    print(answer, 'sum of all readings')
 
 
 def frequency_analysis(t):
+    # find the frequency of each letter in all 10 readings and the frequency of each letter in readings of unique length
     freq = {}
     uniq_freq = {}
+
+    # for each reading
     for reading in t:
+        # for each letter in reading
         for char in reading:
+            # increment count
             freq[char] = freq.get(char, 0) + 1
+
+            # if reading is a unique length
             if len(reading) in (2, 3, 4, 7):
+                # increment count
                 uniq_freq[char] = uniq_freq.get(char, 0) + 1
+
     return freq, uniq_freq
 
 
 if __name__ == '__main__':
+    # map sorted chars to numbers
     numbers = {
         'abcdefg': 8,
         'abdfg': 5,
@@ -80,7 +112,13 @@ if __name__ == '__main__':
         'abcefg': 0,
         'cf': 1
     }
-    frequency = {8: ['a', 'c'], 7: ['g', 'd'], 9: ['f'], 4: ['e'], 6: ['b']}
+    # frequency of each letter
+    frequency = {8: ['a', 'c'],
+                 7: ['g', 'd'],
+                 9: ['f'],
+                 4: ['e'],
+                 6: ['b']
+                 }
     readings = inputs('input.txt')
     part1()
     part2()
